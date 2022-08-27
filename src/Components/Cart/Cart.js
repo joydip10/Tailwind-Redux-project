0b1b98a3117fc481bc79896feed883201fc33452
joydip10 <joydipdasNSTU@gmail.com>
2022-08-27 10:05:49 +0600

    slight changes

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -14,7 +14,7 @@ const Cart = (props) => {
         <div className='sticky'>
             <h1 className='text-amber-400 text-3xl'>Cart</h1>
             <div className='bg-amber-400 p-4 rounded-md shadow-red-600 shadow-lg my-5'>
-                <h1 className='text-2xl text-green-500 text-center border rounded-lg py-1 border-red-500'>Summary</h1>
+                <h1 className='text-2xl text-green-500 text_center border rounded-lg py-1 border-red-500'>Summary</h1>
                 <h1>Quantity: {props.data.length}</h1>
                 <h1>Delivery Charge: 40</h1>
                 <h1 className='text-2xl'>Total: ${totalPrice(props.data,40)}</h1>
@@ -27,7 +27,7 @@ const Cart = (props) => {
             </div>
 
             <div className='bg-amber-400 p-4 rounded-md shadow-red-600 shadow-lg my-5'>
-                <h1 className='text-2xl text-green-500 text-center border rounded-lg py-1 border-red-500'>Orders</h1>
+                <h1 className='text-2xl text-green-500 text_center border rounded-lg py-1 border-red-500'>Orders</h1>
                 <div className='my-3 space-y-2'>
                     {
                         props.data.map(order => <Orders key={order.model+`${Math.random(1,10,0.1)}`} order={order} remove={props.removeFromCartHandler}></Orders>)
